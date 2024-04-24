39283c1492c65c4f3a66768e5453ffc9f94a8a1e
iman <imanimen81@gmail.com>
2024-04-24 14:17:12 +0330

    [UPDATE] kernel.php

--- src/SpeedRoutesServiceProvider.php
+++ src/SpeedRoutesServiceProvider.php
@@ -3,11 +3,18 @@
 namespace Imanimen\SpeedRoutes;
 
 use Illuminate\Support\ServiceProvider;
+use Imanimen\SpeedRoutes\Console\Commands\CreateActionCommand;
 
 class SpeedRoutesServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
+		if ( $this->app->runningInConsole() )
+        {
+            $this->commands( [
+                                 CreateActionCommand::class ,
+                             ] );
+        }
 	}
 
 	public function register()
