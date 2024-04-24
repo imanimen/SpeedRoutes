56785e6be8fcdf197f3270a91399f98db26e06b3
iman <imanimen81@gmail.com>
2024-04-24 12:13:55 +0330

    [UPDATE] service provider

---
+++ src/Providers/ActionRouteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Imanimen\SpeedRoutes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ActionRouteServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+	}
+
+	public function register()
+	{
+		$this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
+	}
+}
