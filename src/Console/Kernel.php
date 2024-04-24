b2dfef36e7cc7916eef8b9f157f59c13d913c337
iman <imanimen81@gmail.com>
2024-04-24 14:10:42 +0330

    [UPDATE] kernel.php

---
+++ src/Console/Kernel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Imanimen\SpeedRoutes\Console;
+
+use Imanimen\SpeedRoutes\Console\Commands\CreateActionCommand;
+use Illuminate\Console\Scheduling\Schedule;
+use Laravel\Lumen\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        CreateActionCommand::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        //
+    }
+}
