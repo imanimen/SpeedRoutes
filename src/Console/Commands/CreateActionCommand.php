b8e98ee157f9ac51449cfc4d3c348ff699490122
iman <imanimen81@gmail.com>
2024-04-26 20:38:14 +0330

    [FEAT] add new methods in cli

--- src/Console/Commands/CreateActionCommand.php
+++ src/Console/Commands/CreateActionCommand.php
@@ -5,7 +5,6 @@ namespace Imanimen\SpeedRoutes\Console\Commands;
 use Exception;
 use Illuminate\Console\Command;
 
-
 class CreateActionCommand extends Command
 {
     /**
@@ -13,7 +12,7 @@ class CreateActionCommand extends Command
      *
      * @var string
      */
-    protected $signature = "make:action {name}";
+    protected $signature = "make:action {name} {--method=GET}";
 
     /**
      * The console command description.
@@ -21,7 +20,6 @@ class CreateActionCommand extends Command
      * @var string
      */
     protected $description = "create actions";
-
 
     /**
      * Execute the console command.
@@ -31,28 +29,30 @@ class CreateActionCommand extends Command
     public function handle()
     {
         try {
-           $action = $this->argument('name');
-           $dir    = base_path() . '/' . 'app/Actions/' . ucfirst($action) . 'Action.php';
-           $actionDir = base_path() . '/' . 'app/Actions/';
-           if (!is_dir($actionDir)) {
-            mkdir($actionDir, 0755, true);
-           }
-           if (file_exists($dir))
-           {
-             $this->error('Action Already Exists!');
-           }
-           else
-	   {
-            $stubPath = __DIR__ . '/../../Stubs/ActionRoute.stub';
-            $stub      = file_get_contents($stubPath);
-            $stub      = str_replace('{{action_name}}', $action, $stub);
+            $action = $this->argument('name');
+            $method = strtoupper($this->option('method'));
+            if (!in_array($method, ['GET', 'POST', 'DELETE', 'PATCH', 'ANY'])) {
+                throw new Exception("Invalid method specified.");
+            }
             
-            $write     = base_path() . '/' . 'app/Actions/' . ucfirst($action) . 'Action.php';
-            file_put_contents($write, $stub);
-            $this->info("Action Created. Action Location: ". $write);
-           }
+            $dir = base_path() . '/app/Actions/' . ucfirst($action) . 'Action.php';
+            $actionDir = base_path() . '/app/Actions/';
+            if (!is_dir($actionDir)) {
+                mkdir($actionDir, 0755, true);
+            }
+            if (file_exists($dir)) {
+                $this->error('Action Already Exists!');
+            } else {
+                $stubPath = __DIR__ . '/../../Stubs/ActionRoute.stub';
+                $stub = file_get_contents($stubPath);
+                $stub = str_replace(['{{action_name}}', '{{method}}'], [$action, $method], $stub);
+                
+                $write = base_path() . '/app/Actions/' . ucfirst($action) . 'Action.php';
+                file_put_contents($write, $stub);
+                $this->info("Action Created. Action Location: " . $write);
+            }
         } catch (Exception $e) {
-            $this->error("An Error Occurred ". $e->getMessage());
+            $this->error("An Error Occurred " . $e->getMessage());
         }
     }
 }
