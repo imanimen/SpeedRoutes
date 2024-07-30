ec09a0ea3260696a91ef5f02b5b1ee0464117746
iman <imanimen81@gmail.com>
2024-07-30 15:21:44 +0330

    feat: adding modular feature

--- src/Console/Commands/CreateActionCommand.php
+++ src/Console/Commands/CreateActionCommand.php
@@ -19,55 +19,59 @@ class CreateActionCommand extends Command
      *
      * @var string
      */
-    protected $description = "create actions";
+    protected $description = "Create actions";
 
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-// ...
+    public function handle()
+    {
+        try {
+            $action = $this->argument('name');
+            $method = strtoupper($this->option('method'));
+            $module = $this->option('module');
 
-public function handle()
-{
-    try {
-        $action = $this->argument('name');
-        $method = strtoupper($this->option('method'));
-        $module = $this->option('module');
+            if (!in_array($method, ['GET', 'POST', 'DELETE', 'PATCH', 'ANY'])) {
+                throw new Exception("Invalid method specified.");
+            }
 
-        if (!in_array($method, ['GET', 'POST', 'DELETE', 'PATCH', 'ANY'])) {
-            throw new Exception("Invalid method specified.");
+            $modulePath = '';
+            $actionDir = '';
+            $namespace = 'App\\Actions';
+
+            if (!empty($module)) {
+                $modulePath = 'Modules/' . ucfirst($module) . '/';
+                $actionDir = base_path($modulePath . 'Actions/');
+                $namespace = 'Modules\\' . ucfirst($module) . '\\Actions';
+            } else {
+                $actionDir = app_path('Actions/');
+            }
+
+            if (!is_dir($actionDir)) {
+                mkdir($actionDir, 0755, true);
+            }
+
+            $dir = $actionDir . ucfirst($action) . 'Action.php';
+
+            if (file_exists($dir)) {
+                $this->error('Action Already Exists!');
+            } else {
+                $stubPath = __DIR__ . '/../../Stubs/ActionRoute.stub';
+                $stub = file_get_contents($stubPath);
+                $stub = str_replace(
+                    ['{{namespace}}', '{{action_name}}', '{{method}}'],
+                    [$namespace, $action, $method],
+                    $stub
+                );
+
+                $write = $actionDir . ucfirst($action) . 'Action.php';
+                file_put_contents($write, $stub);
+                $this->info("Action Created. Action Location: " . $write);
+            }
+        } catch (Exception $e) {
+            $this->error("An Error Occurred: " . $e->getMessage());
         }
-
-        $modulePath = '';
-        $actionDir = '';
-
-        if (!empty($module)) {
-            $modulePath = 'Modules/' . ucfirst($module) . '/';
-            $actionDir = base_path($modulePath . 'Actions/');
-        } else {
-            $actionDir = app_path('Actions/');
-        }
-
-        if (!is_dir($actionDir)) {
-            mkdir($actionDir, 0755, true);
-        }
-
-        $dir = $actionDir . ucfirst($action) . 'Action.php';
-
-        if (file_exists($dir)) {
-            $this->error('Action Already Exists!');
-        } else {
-            $stubPath = __DIR__ . '/../../Stubs/ActionRoute.stub';
-            $stub = file_get_contents($stubPath);
-            $stub = str_replace(['{{action_name}}', '{{method}}'], [$action, $method], $stub);
-
-            $write = $actionDir . ucfirst($action) . 'Action.php';
-            file_put_contents($write, $stub);
-            $this->info("Action Created. Action Location: " . $write);
-        }
-    } catch (Exception $e) {
-        $this->error("An Error Occurred " . $e->getMessage());
     }
-}
 }
